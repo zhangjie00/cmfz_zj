60570fdd879ea7da16aa4a4c27878b7e299d8f03
zhangjie1314520 <2655975304@qq.com>
2023-07-20 02:37:31 +0800

    一次性提交

---
+++ src/test/java/Pig/Pio.java
@@ -0,0 +1,6 @@
+public class Pio{
+
+    public static void main(String[] args) {
+        System.out.println("张杰，江苏欢迎你！");
+    }
+}
---
+++ src/test/java/Pig/Pug.java
@@ -0,0 +1,6 @@
+public class Pug{
+
+    public static void main(String[] args) {
+        System.out.println("张杰，江苏欢迎你！");
+    }
+}
